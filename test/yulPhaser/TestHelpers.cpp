74a189592a3cefd23ca6cf293887111c35aea3b3
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-06-02 15:00:33 +0200

    yul-phaser: Unsigned abs() of a difference of unsigned values

--- test/yulPhaser/TestHelpers.cpp
+++ test/yulPhaser/TestHelpers.cpp
@@ -74,7 +74,10 @@ size_t phaser::test::countDifferences(Chromosome const& _chromosome1, Chromosome
 	for (size_t i = 0; i < min(_chromosome1.length(), _chromosome2.length()); ++i)
 		count += static_cast<size_t>(_chromosome1.optimisationSteps()[i] != _chromosome2.optimisationSteps()[i]);
 
-	return count + abs(static_cast<int>(_chromosome1.length() - _chromosome2.length()));
+	return count + static_cast<size_t>(abs(
+		static_cast<long>(_chromosome1.length()) -
+		static_cast<long>(_chromosome2.length())
+	));
 }
 
 TemporaryDirectory::TemporaryDirectory(std::string const& _prefix):
