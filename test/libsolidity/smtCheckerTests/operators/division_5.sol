5bfe48ae6cda51bf2f422c0249bf387bebebb642
Leonardo Alt <leo@ethereum.org>
2020-06-03 19:40:10 +0200

    [SMTChecker] Add underflow/overflow target to CHC

--- test/libsolidity/smtCheckerTests/operators/division_5.sol
+++ test/libsolidity/smtCheckerTests/operators/division_5.sol
@@ -9,3 +9,7 @@ contract C {
 		return c;
 	}
 }
+// ----
+// Warning: (151-156): Error trying to invoke SMT solver.
+// Warning: (169-179): Condition is always true.
+// Warning: (151-156): Overflow (resulting value larger than 2**256 - 1) happens here
